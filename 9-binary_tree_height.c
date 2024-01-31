26112ba8d83c47c9a9ca9564c86825e93d0fd577
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-01-31 23:32:07 +0100

    changed code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,10 +8,29 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 
-	size_t left_height = 0;
-	size_t right_height = 0;
-    /*one output error*/
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (left_height > right_height ? left_height : right_height);
+	/*size_t left_height = 0;
+	 *size_t right_height = 0;
+     *one output error
+	 *left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	 *right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	 *return (left_height > right_height ? left_height : right_height);
+     */
+    if (!tree || (!tree->left && !tree->right))
+		return (0);
+	return (max(binary_tree_height(tree->left),
+				binary_tree_height(tree->right)) +
+			1);
+}
+
+/**
+ * max - returns the max of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: max of a and b
+ */
+size_t max(size_t a, size_t b)
+{
+	if (a > b)
+		return (a);
+	return (b);
 }

956124b4d022028d9b322cde04820ce567f37e9a
G33GII <gazasolomon@gmail.com>
2024-02-01 23:54:35 +0100

    update

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_size - function that measures the size of a binary tree
  * @tree: pointer to the root node of the tree to measure the size
- *
  * Return: size of the binary tree, 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)

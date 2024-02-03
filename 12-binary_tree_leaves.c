087dfe88647d7d69e10f503b58945463dd8153a6
_ga_za <133493252+G33GII@users.noreply.github.com>
2024-02-03 12:42:08 +0100

    std=gnu89 compliant

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,16 +7,18 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+    size_t left_leaves, right_leaves;
 
-	/* If the node is a leaf, return 1 */
+    if (tree == NULL)
+        return (0);
+
+    /* If the node is a leaf, return 1 */
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	/* Recursively count the leaves in the left and right subtrees */
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
 
 	/* Return the sum of leaves in the left and right subtrees */
 	return (left_leaves + right_leaves);
